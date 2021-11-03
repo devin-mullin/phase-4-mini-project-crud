a29c55d8c0668273f4eb8f0256534f4249c16e4a
Devin Mullin <devin.mullin90@gmail.com>
2021-11-03 18:16:07 -0500

    commit tree 937cbcd1ec2cfc38622a28f512161b0c5834b1f6

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,50 @@
+class SpicesController < ApplicationController
+
+    def index
+        spice = Spice.all
+        render json: spice
+    end
+
+    # def show
+    #     spice = Spice.find_by(id: params[:id])
+    #     render json: spice
+    # rescue ActiveRecord::RecordNotFound
+    #     render_not_found
+    # end
+
+    def create 
+        spice = Spice.create(spice_params)
+        render json: spice, status: :created
+    end
+
+    def update 
+        spice = find_spice
+        spice.update(spice_params)
+        render json: spice
+        rescue ActiveRecord::RecordNotFound
+            render_not_found
+    end
+
+    def destroy
+        spice = find_spice
+        spice.destroy
+        head :no_content
+    rescue ActiveRecord::RecordNotFound
+        render_not_found
+    end
+
+
+    private
+
+    def find_spice
+        Spice.find_by(id: params[:id])
+    end
+
+    def spice_params
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+
+    def render_not_found
+        render json: { error: "Spice not found"}, status: :not_found
+    end
+end
